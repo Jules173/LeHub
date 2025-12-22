9b2e03634db7eb445e4ee28188498e34c23b372f
Jules173 <jules.chuzeville@gmail.com>
2025-12-22 18:35:57 +0100

    V1 finie

--- obj/Debug/net8.0-windows/Views/PresetFormWindow.g.cs
+++ obj/Debug/net8.0-windows/Views/PresetFormWindow.g.cs
@@ -76,7 +76,7 @@ namespace LeHub.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/LeHub;component/views/presetformwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LeHub;V1.0.0.0;component/views/presetformwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\PresetFormWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
